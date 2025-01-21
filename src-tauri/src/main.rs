331fce98dd822e4fa477b1953e2e196aeaaa2382
Mark Howe <mvahowe@gmail.com>
2025-01-21 23:25:32 +0100

    Set up local git

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,31 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::env;
+use pankosmia_web::rocket;
+use tokio::runtime::Runtime;
+use serde_json::json;
+use rocket::fs::relative;
+fn rocket1() -> () {
+    let rt = Runtime::new().unwrap();
+    let args: Vec<String> = env::args().collect();
+    let mut working_dir = "".to_string();
+    if args.len() == 2 {
+        working_dir = args[1].clone();
+    };
+    let webfont_path = relative!("../webfonts");
+    let conf = json!({
+        "working_dir": working_dir,
+        "webfont_path": webfont_path
+    });
+    let builder = rocket(conf);
+    rt.block_on(
+        async move {
+            let _ = builder.launch().await;
+        }
+    );
+}
+fn main() {
+    std::thread::spawn(move || rocket1());
+    pithekos_lib::run()
+}
