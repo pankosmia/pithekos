c1ea525cbb7ed36873753406544d20a1eeede12d
Mark Howe <mvahowe@gmail.com>
2025-01-22 14:15:20 +0100

    Setup for generic deployment

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# pithekos
+# Pithekos
 The Xenizo build of a Pankosmia editor which also acts as a reference use case
+
+## Setup and configuration
+This repo pulls together several libraries and projects into a single Tauri app. The projects are spread across several repos to allow modular reuse. The sanest way to get this working is to install the repos with the following structure:
+
+```
+pankosmia
+-- pithekos
+-- core-client-dashboard
+-- core-client-setup
+-- core-client-workspace
+-- core-client-remote-repos
+```
+
+The `setup` directory contains
+- app_setup.json - this defines the list of clients to be used within Pithekos. Changing this file changed Pithekos for everyone. Keep the variable substitution so that the project works for everyone.
+- local_setup.json - this file is auto-generated the first time you run Pithekos. Do not commit it! You may change the path to your pankosmia directory here if necessary.
+
+Pankosmia-web serves compiled files from the `build` directory of each client, so you need to build each client:
+```
+# In each client repo, NOT this repo!
+npm install
+npm run build
+```
+
+## Running in dev mode
+```
+npm install
+npm run tauri dev
+```
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::env;
+use std::{env, fs};
+use std::path::Path;
+use std::process::exit;
 use pankosmia_web::rocket;
 use tokio::runtime::Runtime;
 use serde_json::json;
 use rocket::fs::relative;
+use std::io::Write;
+use home::home_dir;
+
+fn maybe_os_quoted_path_str(s: String) -> String {
+    match env::consts::OS {
+        "windows" => s.replace("\\", "\\\\").replace("/", "\\\\"),
+        _ => s
+    }
+}
 fn rocket1() -> () {
     let rt = Runtime::new().unwrap();
     let args: Vec<String> = env::args().collect();
@@ -15,9 +26,38 @@ fn rocket1() -> () {
     };
     let webfont_path = relative!("../webfonts");
     let app_setup_path = relative!("../setup/app_setup.json");
+    let local_setup_path = relative!("../setup/local_setup.json");
+    let local_setup_path_exists = Path::new(&local_setup_path).is_file();
+    // Create local setup file if necessary (excluded from git)
+    if !local_setup_path_exists {
+        let stub_local_setup_content = json!({
+            "local_pankosmia_path": format!("{}/{}",
+                home_dir().unwrap().as_os_str().to_str().unwrap().to_string(),
+                "repos/pankosmia"
+            )
+        });
+        let mut file_handle = match fs::File::create(&local_setup_path) {
+            Ok(h) => h,
+            Err(e) => {
+                println!("Could not open local setup file '{}' to write default: {}", local_setup_path, e);
+                exit(1);
+            }
+        };
+        let content_string = maybe_os_quoted_path_str(
+            serde_json::to_string(&stub_local_setup_content).unwrap()
+        );
+        match file_handle.write_all(&content_string.as_bytes()) {
+            Ok(_) => {}
+            Err(e) => {
+                println!("Could not write default local setup file to '{}: {}'", local_setup_path, e);
+                exit(1);
+            }
+        }
+    }
     let conf = json!({
         "working_dir": working_dir,
         "webfont_path": webfont_path,
+        "local_setup_path": local_setup_path,
         "app_setup_path": app_setup_path
     });
     let builder = rocket(conf);
